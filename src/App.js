7a77e0575c2946b3cddd7ca42aef3868afcb5fb2
nareshyadu <nagnari0@gmail.com>
2020-04-19 17:16:22 +0530

    handle change and handle submit code added

--- src/App.js
+++ src/App.js
@@ -11,20 +11,32 @@ import TodoList from './components/TodoList';
 
 class App extends Component {
   state = {
-    items : [
-      {id: 1, title: 'wake up'},
-      {id: 2, title: 'Make up'}
-    ],
+    items : [],
     id:uuid(),
     item:'',
     editItem:false
   };
 
 handleChange = (e) =>{
-  console.log('handle change')
-}
+  this.setState({
+    item: e.target.value
+  });
+};
 handleSubmit = (e) =>{
-  console.log('handle Submit')
+  e.preventDefault();
+  const newItem = {
+    id: this.state.id,
+    title: this.state.item
+  };
+  const updatedItems =
+  [...this.state.items, newItem];
+
+  this.setState({
+    items: updatedItems,
+    item:'',
+    id: uuid(),
+    editItem: false
+  }, () => console.log(this.state))
 }
 clearlist = () =>{
   console.log('clear List')
